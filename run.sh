84c2365c19dbaa40324b0feeb9725d22a6029a9f
Csoka Gyozo <csokagyozo@gmail.com>
2024-02-12 15:52:52 +0100

    adjusted paths

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-docker volume create my_vol
+docker volume create docker-test_my_vol
 
-docker run -v my_vol:/vol --name helper busybox true
-docker cp state.json helper:/vol
-docker cp log.json helper:/vol
+docker run -v docker-test_my_vol:/vol --name helper busybox true
+docker cp change_detector/state.json helper:/vol
+docker cp change_detector/log.json helper:/vol
 docker rm helper
 
 docker-compose up
